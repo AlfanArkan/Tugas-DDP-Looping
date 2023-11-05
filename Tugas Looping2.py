d9b6798c6c19922e2c02ecafe142d2d4963156c4
AlfanArkan <146048492+AlfanArkan@users.noreply.github.com>
2023-11-05 18:34:01 +0700

    Add files via upload

---
+++ Tugas Looping2.py
@@ -0,0 +1,6 @@
+# Meminta input jumlah baris
+jumlah_baris = int(input("Masukkan jumlah baris: "))
+
+# Membuat pola bintang menggunakan perulangan
+for i in range(1, jumlah_baris + 1):
+    print('*' * i)
---
+++ Tugas Looping3.py
@@ -0,0 +1,11 @@
+total = 0
+start_number = 1
+end_number = 19
+increment = 2
+
+
+for i in range(start_number, end_number + 1, increment):
+    total += i
+
+
+print(f"Hasil penjumlahan deret bilangan ganjil dari {start_number} sampai {end_number} adalah {total}.")
